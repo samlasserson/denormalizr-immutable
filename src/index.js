f4fc877ffba2486bd8549f77a370ffaa6baa91c4
André Marques <dehbmarques@gmail.com>
2016-03-15 15:27:41 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import pickBy from 'lodash/pickBy';
+import forIn from 'lodash/forIn';
+import isObject from 'lodash/isObject';
+import { Iterable } from 'immutable';
+
+export default function denormalize(object, entities, schema) {
+
+  if (!Iterable.isIterable(entities)) {
+    throw new Error('Denormalize accepts an immutable object as its entities.');
+  }
+
+  if (!isObject(schema) || Array.isArray(schema)) {
+    throw new Error('Denormalize accepts an object for schema.');
+  }
+
+  if (['EntitySchema', 'ArraySchema'].indexOf(schema.constructor.name) === -1) {
+    throw new Error('Denormalize accepts an EntitySchema or ArraySchema for schema.');
+  }
+
+  if (!object) {
+    return object;
+  }
+
+  if (schema.constructor.name === 'ArraySchema') {
+    return denormalizeArray(object, entities, schema);
+  }
+
+  return denormalizeEntity(object, entities, schema);
+}
+
+function getEntity(id, entities, schema) {
+
+  const entity = entities.getIn([schema.getKey(), `${id}`]);
+  return entity && denormalize(entity, entities, schema);
+}
+
+function denormalizeArray(object, entities, schema) {
+
+  return object.map(entity =>
+    denormalize(entity, entities, schema.getItemSchema())
+  );
+}
+
+function denormalizeEntity(object, entities, schema) {
+
+  const associations = pickBy(schema, key =>
+    ['EntitySchema', 'ArraySchema'].indexOf(key.constructor.name) > -1
+  );
+
+  let denormalized = object;
+  forIn(associations, (_schema, key) => {
+
+    const isArray = _schema.constructor.name === 'ArraySchema';
+
+    if (isArray) {
+      denormalized = denormalized.update(key, ids =>
+        ids && ids.map(id => getEntity(id, entities, _schema.getItemSchema()))
+      );
+    }
+    else {
+      denormalized = denormalized.update(key, id => getEntity(id, entities, _schema));
+    }
+  });
+
+  return denormalized;
+}
---
+++ test/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var should = require('chai').should();
+var range = require('lodash/range');
+var sample = require('lodash/sample');
+
+var denormalize = require('../src').default;
+
+var immutable = require('immutable');
+var fromJS = immutable.fromJS;
+var Map = immutable.Map;
+
+var normalizr = require('normalizr');
+var normalize = normalizr.normalize;
+var Schema = normalizr.Schema;
+var arrayOf = normalizr.arrayOf;
+
+describe('denormalizr', function () {
+
+  var baseSchema = {
+    article: new Schema('articles'),
+    author: new Schema('authors'),
+    comment: new Schema('comments')
+  };
+  baseSchema.article.define({
+    author: baseSchema.author
+  });
+  baseSchema.comment.define({
+    author: baseSchema.author
+  });
+  baseSchema.article.define({
+    comments: arrayOf(baseSchema.comment)
+  });
+
+  it('fails denormalizing without an object schema', function () {
+    (function () {
+      denormalize(undefined, new Map(), null);
+    }).should.throw();
+  });
+
+  it('fails denormalizing with an invalid schema', function () {
+    (function () {
+      denormalize(undefined, new Map(), { });
+    }).should.throw();
+  });
+
+  it('fails denormalizing without entities', function () {
+    (function () {
+      denormalize(undefined, undefined, { });
+    }).should.throw();
+  });
+
+  it('fails denormalizing with non immutable entities', function () {
+    (function () {
+      denormalize(undefined, { }, { });
+    }).should.throw();
+  });
+
+  it('can denormalize a single entity', function () {
+
+    var articles = generateArticles();
+    Object.freeze(articles);
+    var normalized = normalize(articles, arrayOf(baseSchema.article));
+    var entities = fromJS(normalized.entities);
+
+    var article = entities.get('articles').get('0');
+    var denormalized = denormalize(article, entities, baseSchema.article);
+
+    denormalized.toJS().should.eql(articles[0]);
+  });
+
+  it('can denormalize an array', function () {
+
+    var articles = generateArticles();
+    Object.freeze(articles);
+    var normalized = normalize(articles, arrayOf(baseSchema.article));
+    var entities = fromJS(normalized.entities);
+
+    var denormalized = denormalize(entities.get('articles'), entities, arrayOf(baseSchema.article));
+
+    denormalized.toList().toJS().should.eql(articles);
+  });
+
+});
+
+
+function generateArticles() {
+  return range(3).map(function (articleId) {
+    return {
+      id: articleId,
+      title: `Article ${articleId}`,
+      author: {
+        id: articleId,
+        name: `Author ${articleId}`
+      },
+      comments: range(3).map(function (commentId) {
+        commentId += (articleId * 3);
+        const authorId = sample([0, 1, 2]);
+        return {
+          id: commentId,
+          message: `Comment ${commentId}`,
+          author: {
+            id: authorId,
+            name: `Author ${authorId}`
+          }
+        }
+      })
+    };
+  });
+}
